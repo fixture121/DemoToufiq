7db318fc9e3afec3d5626e5e7531d8e02799e85d
fixture121 <toufiqcharania@outlook.com>
2025-03-11 17:08:54 -0400

    Added Task 3

--- src/main/java/com/sheridan/App.java
+++ src/main/java/com/sheridan/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World, this is Toufiq Charania!" );
         System.out.println("This is Task 1");
         System.out.println("This is Task 2");
+        System.out.println("This is Task 3");
     }
 }
