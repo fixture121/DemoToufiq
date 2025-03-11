a699fad77d5ff7a88c46141a0c7312f86abc3808
fixture121 <toufiqcharania@outlook.com>
2025-03-11 16:56:18 -0400

    Added Task 1

--- src/main/java/com/sheridan/App.java
+++ src/main/java/com/sheridan/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World, this is Toufiq Charania!" );
+        System.out.println("This is Task 1");
     }
 }
